69d1a51d7782f2c8da39e95ad62079ffd2602ded
Christopher Soden <karavol27@gmail.com>
2016-08-02 10:16:39 -0600

    Creating bestbuy api call

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -4,6 +4,15 @@ class BestBuyService
     @connection = Faraday.new(url: "https://api.bestbuy.com/v1/products")
   end
 
-  
+  def retrieve_stores(zip)
+    conn = Faraday.new(url: "http://api.bestbuy.com/v1/stores")
+    https://api.bestbuy.com/v1/stores((area(80203,25)))?apiKey=&show=longName,phone,storeType&pageSize=15&callback=JSON_CALLBACK&format=json
+
+
+    @connection.get do |r|
+      r.url "%28%28area%2880203,25%29%29%29"
+      r.params['apiKey'] = ENV['best_bu']
+    end
+  end
 
 end
--- spec/features/best_buy_story_1_spec.rb
+++ spec/features/best_buy_story_1_spec.rb
@@ -6,14 +6,22 @@ RSpec.feature "1st best buy story" do
     # When I visit "/"
     visit root_path
     # And I fill in a search box with "80202" and click "search"
-    fill_in "search" with: "80202"
+    fill_in "search", with: "80202"
+    click_button "Search"
     # Then my current path should be "/search" (ignoring params)
     expect(current_path).to eq('/search')
     # And I should see stores within 25 miles of 80202
     # And I should see a message that says "17 Total Stores"
+    expect(page).to have_content("17 Total Stores")
 
-    expect(page).to have_css(".store", :count => 17)
     # And I should see exactly 15 results
+    expect(page).to have_css(".store", :count => 15)
     # And I should see the long name, city, distance, phone number and store type for each of the 15 results
+    within("#store-1") do
+      expect(page).to have_css(".long-name")
+      expect(page).to have_css(".distance")
+      expect(page).to have_css(".phone-number")
+      expect(page).to have_css(".store-type")
+    end
   end
 end
