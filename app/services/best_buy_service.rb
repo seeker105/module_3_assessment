c685ec02f737b6eb8f59902691b7dccecf811c6e
Christopher Soden <karavol27@gmail.com>
2016-08-02 11:16:33 -0600

    commit'

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -4,12 +4,25 @@ class BestBuyService
     conn = Faraday.new(url: "http://api.bestbuy.com/v1/stores%28%28area%28#{zip},25%29%29%29")
     response = conn.get do |r|
       r.params['apiKey'] = ENV['best_buy_api_key']
-      r.params['show'] = 'longName,phone,storeType,distance'
+      r.params['show'] = 'longName,phone,storeType,distance,storeId'
       r.params['pageSize'] = '15'
       r.params['format'] = 'json'
-      r.params['storeId']
     end
     result = JSON.parse(response.body, symbolize_names: true)[:stores]
+  end
+
+  def retrieve_store(id)
+    # https://api.bestbuy.com/v1/stores((storeId=281))=&pageSize=15&callback=JSON_CALLBACK&format=json
+    conn = Faraday.new(url: "http://api.bestbuy.com/v1/stores%28%28storeId=#{id}%29%29")
+    response = conn.get do |r|
+      r.params['apiKey'] = ENV['best_buy_api_key']
+      r.params['show'] = 'longName,phone,storeType,address,city,region,fullPostalCode,hours'
+      r.params['format'] = 'json'
+    end
+    result = JSON.parse(response.body, symbolize_names: true)[:stores]
+    result[0][:hours_split]=result[0][:hours].split('\; ')
+    byebug
+    return result
   end
 
 end
